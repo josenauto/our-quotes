5ef37a5857005a4e8197149eaeb5b8005a933d80
josenauto <jemilionautoch@gmail.com>
2020-12-22 17:29:16 -0300

    Created the file with the data

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const quotes = [
+  {
+    id: 1,
+    name: "jorge luis borges",
+    country: "argentina",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Jorge_Luis_Borges.jpg/220px-Jorge_Luis_Borges.jpg",
+    text:
+      "Yo no hablo de venganzas ni perdones; el olvido es la única venganza y el único perdón.",
+  },
+  {
+    id: 2,
+    name: "eduardo galeano",
+    country: "uruguay",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/b/bf/Eduardo_Galeano_ltk_%28cropped%29.jpg",
+    text:
+      "Ahora yo no sé si vas a poder leer esta carta, pero igual siento como una necesidad de decirte que yo contigo he sido más feliz de lo que los libros dicen que se puede.",
+  },
+  {
+    id: 3,
+    name: "ernesto sabato",
+    country: "argentina",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/ErnestoSabato001.JPG/180px-ErnestoSabato001.JPG",
+    text:
+      "Siempre es levemente siniestro volver a los lugares que han sido testigos de un instante de perfección.",
+  },
+  {
+    id: 4,
+    name: "gabriela mistral",
+    country: "chile",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Gabriela_Mistral_1945.jpg/220px-Gabriela_Mistral_1945.jpg",
+    text: "Los días más felices son aquellos que nos hacen sabios.",
+  },
+];
+
+export default quotes;
