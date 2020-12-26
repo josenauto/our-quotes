128938bcfab45bd1add5fbfc65855aa6b4b115ce
josenauto <jemilionautoch@gmail.com>
2020-12-26 17:41:32 -0300

    Imported a hook and modified the component

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import authors from "./data";
 
 const Quotes = () => {
-  return <h2>Quotes component</h2>;
+  const [index, setIndex] = useState(0)
+  const {name, country, image, text } = authors[index];
+  return (
+    <section>
+      <div>
+        <img src={image} alt={name} />
+
+      </div>
+      <h4>{name}</h4>
+      <p>{country}</p>
+      <p>{text}</p>
+    </section>
+  );
 };
 
 export default Quotes;
