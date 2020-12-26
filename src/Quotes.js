750f3a267352f7b18b791c8fde0df4570997e341
josenauto <jemilionautoch@gmail.com>
2020-12-26 18:22:19 -0300

    Added the buttons

--- src/Quotes.js
+++ src/Quotes.js
@@ -8,11 +8,15 @@ const Quotes = () => {
     <section>
       <div>
         <img src={image} alt={name} />
-
       </div>
       <h4>{name}</h4>
       <p>{country}</p>
       <p>{text}</p>
+      <div className="button-container">
+        <button className="prev-btn">&#x2b9c;</button>
+        <button className="next-btn">&#x2b9e;</button>
+      </div>
+      <button className="random-btn">Random</button>
     </section>
   );
 };
